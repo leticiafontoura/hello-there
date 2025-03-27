472bb5f63c5389766d2fa142fd9768c5e6124936
leticiafontoura <leticia.fontoura@live.com>
2025-03-27 05:40:41 -0300

    navbar mobile

--- src/shared/ui/Navigation/NavigationBar.tsx
+++ src/shared/ui/Navigation/NavigationBar.tsx
@@ -2,21 +2,32 @@ import { NavLink } from 'react-router'
 import './NavigationBar.scss'
 
 import HelloThereLogo from '../../../assets/svgs/navbar/hello-there.svg?react'
+import LightSaber from '../../../assets/svgs/lightsaber.svg?react'
 import { INavigationLinks } from '../Header/Header'
+import { useState } from 'react'
 
 interface Props {
   navLinks: Array<INavigationLinks>
 }
 
 function NavigationBar({ navLinks }: Props) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
     <nav className='main-nav'>
-      <span className='main-nav-logo'>
-        <NavLink to='/' title='home'>
-          <HelloThereLogo height={45} aria-label='hello there logo' />
-        </NavLink>
-      </span>
-      <ul>
+      <div className='logo-area'>
+        <button
+          className='mobile-menu'
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <LightSaber />
+        </button>
+        <span className='main-nav-logo'>
+          <NavLink to='/' title='home'>
+            <HelloThereLogo height={45} aria-label='hello there logo' />
+          </NavLink>
+        </span>
+      </div>
+      <ul className={isMenuOpen ? 'nav-open' : ''}>
         {navLinks.map(({ href, text, icon }) => (
           <li key={href}>
             <NavLink
